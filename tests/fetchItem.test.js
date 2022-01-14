5be13fc8055f5710627a1d89632697947a0adaa7
agsleonardo <agsleonardo@hotmail.com>
2022-01-14 17:38:14 -0300

    requirement 9.1 ok

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -3,6 +3,9 @@ const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
 
 describe('2 - Teste a função fecthItem', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  
+  it('Teste se fetchProducts é uma função', () => {
+    expect(typeof fetchItem).toBe('function');
+  })
+
 });
