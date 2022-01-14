8770bcceda9a899e9b90fb309ab5edfe5ed0dd33
agsleonardo <agsleonardo@hotmail.com>
2022-01-14 17:04:47 -0300

    add error call fetchProducts without parameter

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,6 +1,6 @@
 const fetchProducts = (query) => {
   const END_POINT = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
-
+  if (!query) throw new Error('You must provide an url');
   return fetch(END_POINT)
   .then((res) => res.json())
   .catch((err) => console.log(err));
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -25,8 +25,8 @@ describe('1 - Teste a função fecthProducts', () => {
 
   })
 
-  // it('Teste se fetchProducts é uma função', () => {
-  //   expect(typeof fetchProducts).toBe('function')
+  // it(`Teste se, ao chamar a função fetchProducts sem argumento, retorna um erro com a mensagem: You must provide an url. Dica: Lembre-se de usar o new Error('mensagem esperada aqui') para comparar com o objeto retornado da API.`, () => {
+    
 
   // })
 });
