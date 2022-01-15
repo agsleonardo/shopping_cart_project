b7ea7e29d2146ae9b3d32cb9e56ceb3152d7b3b8
agsleonardo <agsleonardo@hotmail.com>
2022-01-15 15:06:08 -0300

    refac saveCartItems

--- helpers/saveCartItems.js
+++ helpers/saveCartItems.js
@@ -1,6 +1,4 @@
-const saveCartItems = (item) => {
-  localStorage.setItem('cartItems', JSON.stringify(item));
-};
+const saveCartItems = (item) => localStorage.setItem('cartItems', JSON.stringify(item));
 
 if (typeof module !== 'undefined') {
   module.exports = saveCartItems;
