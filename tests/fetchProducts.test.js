d6a88e40079f61741fcb5d2fdc942969bf259740
agsleonardo <agsleonardo@hotmail.com>
2022-01-15 11:41:51 -0300

    requirement 8.3 ok

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -14,9 +14,10 @@ describe('1 - Teste a função fecthProducts', () => {
   })
 
   
-  // it('Teste se, ao chamar a função fetchProducts com o argumento "computador", a função fetch utiliza o endpoint "https://api.mercadolibre.com/sites/MLB/search?q=computador";', () => {
-
-  // })
+  it('Teste se, ao chamar a função fetchProducts com o argumento "computador", a função fetch utiliza o endpoint "https://api.mercadolibre.com/sites/MLB/search?q=computador";', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+  })
 
   it('Teste se o retorno da função fetchProducts com o argumento "computador" é uma estrutura de dados igual ao objeto computadorSearch, que já está importado no arquivo.', async () => {
     const data = await fetchProducts('computador'); 
