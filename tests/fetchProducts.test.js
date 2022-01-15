7aaf93771cc68f30a93393af131b7e61dca57603
agsleonardo <agsleonardo@hotmail.com>
2022-01-15 11:40:00 -0300

    requirement 8.2

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -8,9 +8,10 @@ describe('1 - Teste a função fecthProducts', () => {
     expect(typeof fetchProducts).toBe('function');
   })
 
-  // it('Execute a função fetchProducts com o argumento "computador" e teste se fetch foi chamada', async () => {
-
-  // })
+  it('Execute a função fetchProducts com o argumento "computador" e teste se fetch foi chamada', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
+  })
 
   
   // it('Teste se, ao chamar a função fetchProducts com o argumento "computador", a função fetch utiliza o endpoint "https://api.mercadolibre.com/sites/MLB/search?q=computador";', () => {
