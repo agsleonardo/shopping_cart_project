9591bfd8b34b58600cfd8fd67aa1fb970be923b7
agsleonardo <agsleonardo@hotmail.com>
2022-01-15 11:43:24 -0300

    requirement 9.2 ok

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -8,9 +8,10 @@ describe('2 - Teste a função fecthItem', () => {
     expect(typeof fetchItem).toBe('function');
   })
 
-  // it('Execute a função fetchItem com o argumento do item "MLB1615760527" e teste se fetch foi chamada', () => {
-
-  // })
+  it('Execute a função fetchItem com o argumento do item "MLB1615760527" e teste se fetch foi chamada', async () => {
+    await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalled();
+  })
 
   // it('Teste se, ao chamar a função fetchItem com o argumento do item "MLB1615760527", a função fetch utiliza o endpoint "https://api.mercadolibre.com/items/MLB1615760527"', () => {
 
