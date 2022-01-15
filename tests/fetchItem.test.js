8c59e80ee2a8f2334e3f279919a792e706c9054f
agsleonardo <agsleonardo@hotmail.com>
2022-01-15 11:44:44 -0300

    requirement 9.3

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -13,9 +13,10 @@ describe('2 - Teste a função fecthItem', () => {
     expect(fetch).toHaveBeenCalled();
   })
 
-  // it('Teste se, ao chamar a função fetchItem com o argumento do item "MLB1615760527", a função fetch utiliza o endpoint "https://api.mercadolibre.com/items/MLB1615760527"', () => {
-
-  // })
+  it('Teste se, ao chamar a função fetchItem com o argumento do item "MLB1615760527", a função fetch utiliza o endpoint "https://api.mercadolibre.com/items/MLB1615760527"', async () => {
+    await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/items/MLB1615760527');
+  })
 
   it('Teste se o retorno da função fetchItem com o argumento do item "MLB1615760527" é uma estrutura de dados igual ao objeto item que já está importado no arquivo.', async () => {
     const data = await fetchItem('MLB1615760527')
